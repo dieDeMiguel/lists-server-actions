4739280de3e96f0418960710f9728873f6e5686a
Progress with the component <deMiguel@newsaktuell.de>
2024-11-17 10:14:03 +0100

    Enhance SearchInput component: add debounce functionality and improve loading state handling

--- src/app/search-input.tsx
+++ src/app/search-input.tsx
@@ -2,7 +2,7 @@
 
 import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
 import { useRouter } from "next/navigation";
-import { useTransition } from "react";
+import { useState, useTransition } from "react";
 import { Spinner } from "./components/spinner";
 
 export default function SearchInput({
@@ -12,6 +12,8 @@ export default function SearchInput({
 }) {
   const router = useRouter();
   const [isPending, startTransition] = useTransition();
+  const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | undefined>();
+  const isSearching = isPending || timeoutId;
   return (
     <div className="relative rounded-md shadow-sm border border-gray-300 h-8 items-center flex">
       <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
@@ -28,16 +30,21 @@ export default function SearchInput({
         placeholder="Search"
         defaultValue={search}
         onChange={(e) => {
-          startTransition(() => {
-            if (e.target.value === "") {
-              router.replace("/");
-              return;
-            }
-            router.push(`/?search=${e.target.value}`);
-          });
+          clearTimeout(timeoutId);
+          const id = setTimeout(() => {
+            startTransition(() => {
+              if (e.target.value === "") {
+                router.replace("/");
+                return;
+              }
+              router.push(`/?search=${e.target.value}`);
+            });
+            setTimeoutId(undefined);
+          }, 500);
+          setTimeoutId(id);
         }}
       />
-      {isPending && (
+      {isSearching && (
         <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
           <Spinner
             className="h-5 w-5 animate-spin text-gray-400"
