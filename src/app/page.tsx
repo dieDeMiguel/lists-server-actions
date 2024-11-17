372aa73b0876c380a2dce542011431d555ece282
Progress with the component <deMiguel@newsaktuell.de>
2024-11-17 09:46:56 +0100

    Refactor Users component: add loading state with Spinner and improve search input styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,41 @@ import Link from "next/link";
 import { prisma } from "../../lib/prisma";
 import { ChevronRightIcon } from "@heroicons/react/20/solid";
 import SearchInput from "./search-input";
+import { Suspense } from "react";
+import { Spinner } from "./components/spinner";
 
 export default async function Users({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+  const params = await searchParams;
+  const search = typeof params.search === "string" ? params.search : undefined;
+
+  return (
+    <div className="bg-gray-50 min-h-screen px-4 sm:px-6 lg:px-8 pt-12 flex flex-col items-center justify-center">
+      <div className="w-full sm:w-[600px] md:w-[700px] lg:w-[900px]">
+        <div className="flex items-center justify-between">
+          <div className="w-full sm:w-80">
+            <SearchInput search={search} />
+          </div>
+          <div className="mt-0 sm:ml-4 flex-none">
+            <button
+              type="button"
+              className="block rounded-md bg-indigo-600 py-1.5 px-3 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Add user
+            </button>
+          </div>
+        </div>
+      </div>
+      <Suspense fallback={<Loading />}>
+        <UsersTable searchParams={searchParams} />
+      </Suspense>
+    </div>
+  );
+}
+async function UsersTable({
   searchParams,
 }: {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
@@ -42,96 +75,83 @@ export default async function Users({
   if (page > 1) {
     currentSearchParams.set("page", page.toString());
   }
-
   return (
-    <div className="bg-gray-50 min-h-screen px-4 sm:px-6 lg:px-8 pt-12 flex flex-col items-center justify-center">
-      <div className="w-full sm:w-[600px] md:w-[700px] lg:w-[900px]">
-        <div className="flex items-center justify-between">
-          <div className="w-full sm:w-80">
-            <SearchInput search={search} />
-          </div>
-          <div className="mt-0 sm:ml-4 flex-none">
-            <button
-              type="button"
-              className="block rounded-md bg-indigo-600 py-1.5 px-3 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Add user
-            </button>
-          </div>
-        </div>
-        <div className="mt-8 flow-root">
-          <div className="-my-2 -mx-6">
-            <div className="inline-block min-w-full py-2 align-middle px-6">
-              <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 rounded-lg">
-                <table className="min-w-full divide-y divide-gray-300">
-                  <thead className="bg-gray-50">
-                    <tr>
-                      <th className="py-3.5 pr-3 text-left text-sm font-semibold w-[62px] sm:w-auto text-gray-900 pl-4">
-                        ID
-                      </th>
-                      <th className="px-3 py-3.5 text-left text-sm font-semibold w-[130px] sm:w-auto text-gray-900">
-                        Name
-                      </th>
-                      <th className="px-3 py-3.5 text-left text-sm font-semibold w-[175px] sm:w-auto text-gray-900">
-                        Email
-                      </th>
-                      <th className="relative py-3.5 pl-3 pr-4">
-                        <span className="sr-only">Edit</span>
-                      </th>
+    <div className="w-full sm:w-[600px] md:w-[700px] lg:w-[900px]">
+      <div className="mt-8 flow-root">
+        <div className="-my-2 -mx-6">
+          <div className="inline-block min-w-full py-2 align-middle px-6">
+            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 rounded-lg">
+              <table className="min-w-full divide-y divide-gray-300">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th className="py-3.5 pr-3 text-left text-sm font-semibold w-[62px] sm:w-auto text-gray-900 pl-4">
+                      ID
+                    </th>
+                    <th className="px-3 py-3.5 text-left text-sm font-semibold w-[130px] sm:w-auto text-gray-900">
+                      Name
+                    </th>
+                    <th className="px-3 py-3.5 text-left text-sm font-semibold w-[175px] sm:w-auto text-gray-900">
+                      Email
+                    </th>
+                    <th className="relative py-3.5 pl-3 pr-4">
+                      <span className="sr-only">Edit</span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-200 bg-white">
+                  {users.map((user) => (
+                    <tr key={user.id}>
+                      <td className="whitespace-nowrap py-4 pr-3 text-sm font-medium text-gray-900 pl-4">
+                        {user.id}
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500 font-medium max-w-[130px] sm:w-auto truncate">
+                        {user.name}
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500 max-w-[175px] sm:w-auto truncate">
+                        {user.email}
+                      </td>
+                      <td className="relative whitespace-nowrap py-4 pl-4 pr-4 text-right text-sm font-medium">
+                        <a
+                          href="#"
+                          className="text-indigo-600 hover:text-indigo-900 inline-flex items-center"
+                        >
+                          Edit
+                          <ChevronRightIcon className="w-4 h-4 ml-1" />
+                        </a>
+                      </td>
                     </tr>
-                  </thead>
-                  <tbody className="divide-y divide-gray-200 bg-white">
-                    {users.map((user) => (
-                      <tr key={user.id}>
-                        <td className="whitespace-nowrap py-4 pr-3 text-sm font-medium text-gray-900 pl-4">
-                          {user.id}
-                        </td>
-                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500 font-medium max-w-[130px] sm:w-auto truncate">
-                          {user.name}
-                        </td>
-                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500 max-w-[175px] sm:w-auto truncate">
-                          {user.email}
-                        </td>
-                        <td className="relative whitespace-nowrap py-4 pl-4 pr-4 text-right text-sm font-medium">
-                          <a
-                            href="#"
-                            className="text-indigo-600 hover:text-indigo-900 inline-flex items-center"
-                          >
-                            Edit
-                            <ChevronRightIcon className="w-4 h-4 ml-1" />
-                          </a>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
+                  ))}
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
-        <div className="mt-4 flex items-center justify-between">
-          <p className="text-sm text-gray-700">
-            Showing{" "}
-            <span className="font-semibold">{(page - 1) * perPage + 1}</span> to{" "}
-            <span className="font-semibold">
-              {Math.min(page * perPage, totalUsers)}
-            </span>{" "}
-            of <span className="font-semibold">{totalUsers}</span> users
-          </p>
-          <div className="space-x-2">
-            <PreviousLink
-              currentSearchParams={currentSearchParams}
-              page={page}
-            />
-            <NextLink
-              currentSearchParams={currentSearchParams}
-              page={page}
-              totalPages={totalPages}
-            />
-          </div>
+      </div>
+      <div className="mt-4 flex items-center justify-between">
+        <p className="text-sm text-gray-700">
+          Showing{" "}
+          <span className="font-semibold">{(page - 1) * perPage + 1}</span> to{" "}
+          <span className="font-semibold">
+            {Math.min(page * perPage, totalUsers)}
+          </span>{" "}
+          of <span className="font-semibold">{totalUsers}</span> users
+        </p>
+        <div className="space-x-2">
+          <PreviousLink currentSearchParams={currentSearchParams} page={page} />
+          <NextLink
+            currentSearchParams={currentSearchParams}
+            page={page}
+            totalPages={totalPages}
+          />
         </div>
       </div>
-      .
+    </div>
+  );
+}
+function Loading() {
+  return (
+    <div className="flex h-full items-center grow justify-center">
+      <Spinner className="w-8 animate-spin" />
     </div>
   );
 }
--- src/app/search-input.tsx
+++ src/app/search-input.tsx
@@ -10,7 +10,8 @@ export default function SearchInput({
 }) {
   const router = useRouter();
   return (
-    <div className="relative mt-1 rounded-md shadow-sm">
+    <div className="relative rounded-md shadow-sm border border-gray-300">
+      {" "}
       <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
         <MagnifyingGlassIcon
           className="h-5 w-5 text-gray-400"
@@ -21,7 +22,7 @@ export default function SearchInput({
         type="text"
         name="search"
         id="search"
-        className="block text-black w-full rounded-md border-gray-300 pl-10 focus:ring-0 focus:border-gray-400 focus:outline-none text-sm"
+        className="block text-black w-full rounded-md border-gray-300 pl-10 text-sm focus:border-gray-400 focus:outline-none focus:ring-0"
         placeholder="Search"
         defaultValue={search}
         onChange={(e) => {
